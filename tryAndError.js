43596cf6051725c91ada97bb1623cbb60e591388
aljosh1 <alabijoshib@gmail.com>
2025-09-09 09:56:53 +0100

    testSales

---
+++ tryAndError.js
@@ -0,0 +1,41 @@
+
+// getElementsByClassName("displayA")
+function hover1(){
+    let text = document.getElementsByClassName("displayA")[0]
+    if (text.style.display ==="none"){
+        text.style.display = "block"
+    }
+    else {text.style.display = "none"}
+}
+
+function hover2(){
+    let text = document.getElementsByClassName("displayc")[0]
+    if (text.style.display ==="none"){
+        text.style.display = "block"
+    }
+    else {text.style.display = "none"}
+}
+
+function hover3(){
+    let text = document.getElementsByClassName("displayD")[0]
+    if (text.style.display ==="none"){
+        text.style.display = "block"
+    }
+    else {text.style.display = "none"}
+}
+
+function hover4(){
+    let text = document.getElementsByClassName("displayE")[0]
+    if (text.style.display ==="none"){
+        text.style.display = "block"
+    }
+    else {text.style.display = "none"}
+}
+
+
+
+
+
+
+
+
